f42ac512ae2b4a9f6890b060ccb33e5ebc4e9fe4
voiha <killzone296@gmail.com>
2024-07-01 13:21:15 +0300

    add-randomMath

--- js/index.js
+++ js/index.js
@@ -4,3 +4,20 @@
     $document.ready(() => {
     })
 })();
+
+function getRandomReading() {
+    return (Math.random() * 100).toFixed(2); // Generate a random number between 0 and 100
+}
+
+function updateReadings() {
+    document.getElementById('light1').innerText = getRandomReading() + ' lx'; // Light reading
+    document.getElementById('co2-1').innerText = getRandomReading() + ' ppm'; // CO2 reading
+    document.getElementById('sensorTemp').innerText = getRandomReading() + ' °C'; // Temperature reading
+    document.getElementById('sensorHumidity').innerText = getRandomReading() + ' %'; // Humidity reading
+}
+
+// Update readings every 5 seconds
+setInterval(updateReadings, 2000);
+
+// Initial update
+updateReadings();
